6f0e3887e47eb014023c1f468a7920ff34e88f7a
adhukka <46287602+adhukka@users.noreply.github.com>
2019-05-24 12:07:21 -0500

    Update Reserves_Util.R

--- R/Reserves_Util.R
+++ R/Reserves_Util.R
@@ -1,17 +1,12 @@
-# Hello, world!
+# Reserves_Util Package
 #
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
+# Group of useful function for reserves work.
 #
 #   Build and Reload Package:  'Ctrl + Shift + B'
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
+
+
 library(RODBC)
 library(magrittr)
 library(dplyr)
